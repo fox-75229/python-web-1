96b61f34bde000f4409db250168c0d68c3bd0a00
Jeremy_yu <a0976548925@gmail.com>
2025-10-02 16:46:06 +0800

    這是10/2第七次提交

--- lesson5/app.py
+++ lesson5/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
+
+from flask import Flask,render_template
 
 app = Flask(__name__)
-print(__name__)
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/machine")
+def machine():
+    return render_template("machine.html") 
 
 def main():
     """啟動應用（教學用：啟用 debug 模式）"""
