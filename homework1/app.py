6d7c71d5860f086c12d2fefff03675559e477294
fox-75229 <a0976548925@gmail.com>
2025-10-11 19:58:17 +0800

    美化到一半

---
+++ homework1/app.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/regression")
+def regression():
+    return render_template("regression.html")
+
+@app.route("/knn")
+def knn():
+    return render_template("knn.html")
+
+
+# ===新增 /decision_tree 路由===
+@app.route("/decision_tree")
+def decision_tree():
+    """
+    這個函式會渲染 decision_tree.html 頁面。
+    """
+    return render_template("decision_tree.html")
+# ==================================================
+
+@app.route("/lesson6_1")
+def lesson6_1():
+    page_tile = "我的首頁Robert"
+    users = [
+        {"name": "小明", "is_vip": True},
+        {"name": "小華", "is_vip": False},
+        {"name": "小英", "is_vip": True}
+    ]
+    return  render_template("lesson6_1.html",title=page_tile, user_list = users)
+
+def main():
+    """啟動應用（教學用：啟用 debug 模式）"""
+    # 在開發環境下使用 debug=True，部署時請關閉
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    main()
