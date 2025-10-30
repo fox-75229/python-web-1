3abd9ed9ca81b0d8d59bcc245543b75b30e136eb
Jeremy_yu <a0976548925@gmail.com>
2025-10-30 15:18:35 +0800

    knn

--- lesson11/knn/knn.py
+++ lesson11/knn/knn.py
@@ -1,11 +1,9 @@
 from flask import Blueprint, render_template
-
 knn_bp = Blueprint(
     'knn',__name__,
     url_prefix='/knn',
     template_folder='../templates'
 )
-
 @knn_bp.route('/index')
 def knn_index():
     return render_template('knn.html')
