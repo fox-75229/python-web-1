e8ac613ab12f16953ede46390932768f96405b44
Jeremy_yu <a0976548925@gmail.com>
2025-10-30 16:36:14 +0800

    56

--- lesson11/knn/app.py
+++ lesson11/knn/app.py
@@ -1,4 +1,9 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,jsonify
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
 
 knn_bp = Blueprint(
     'knn',
@@ -11,3 +16,16 @@ knn_bp = Blueprint(
 @knn_bp.route('/knn_index')
 def knn_index():
     return render_template('knn.html')
+
+@knn_bp.route('/api/data')
+def knn_data():
+    """KNN API - 使用鳶尾花資料集"""
+    # 載入鳶尾花資料集
+    iris = load_iris()
+    print(iris)
+    
+    return jsonify(
+        {
+            'success': True
+        }
+    )
